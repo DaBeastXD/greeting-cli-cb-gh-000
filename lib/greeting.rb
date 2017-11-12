d2745c1e5b0c2852b34ccf37271e3a84fbfea1b9
Kevin Le <17786629@student.westernsydney.edu.au>
2017-11-12 07:30:26 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,6 @@
 # code the #greeting method here!
-learn spec/01_greeting_spec.rb
+learn spec/01_greeting_spec.#!/usr/bin/env ruby -wKU
+
 def greeting(name)
 puts "Hello #{name} . It's nice to meet you"
 end
